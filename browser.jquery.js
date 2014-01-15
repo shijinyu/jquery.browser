654cd0afe6e4e054f210b6568a6bf60ecbd98030
Shijinyu <sjym90@gmail.com>
2014-01-15 11:44:52 +0800

    Create browser.jquery.js

---
+++ browser.jquery.js
@@ -0,0 +1,81 @@
+/*
+* author :shi
+*/
+
+$.extend({
+        browser:function(){
+            var ua=window.navigator.userAgent,
+            ret="";
+            if(/Firefox/g.test(ua)){
+                ua=ua.split(" ");
+            ret="Firefox|"+ua[ua.length-1].split("/")[1];
+            }else if(/MSIE/g.test(ua)){
+                ua=ua.split(";");
+                ret="IE|"+ua[1].split(" ")[2];
+            }else if(/Opera/g.test(ua)){
+                ua=ua.split(" ");
+                ret="Opera|"+ua[ua.length-1].split("/")[1];
+            }else if(/Chrome/g.test(ua)){
+                ua=ua.split(" ");
+                ret="Chrome|"+ua[ua.length-2].split("/")[1];
+            }else if(/^apple\s+/i.test(navigator.vendor)){
+                ua=ua.split(" ");
+                ret="Safari|"+ua[ua.length-2].split("/")[1];
+            }else{
+                ret="unknown";
+            }
+         return ret.split("|");
+        }
+    });
+    $.extend($.browser,{
+        ie6:function(){
+            if(typeof document.body.style.maxHeight === 'undefined'){
+                return true;
+            }
+            return false;
+        },
+        msie:function(){
+            return $.browser()[0] =='IE';
+        },
+        moz:function(){
+            return $.browser()[0] =='Firefox';
+        },
+        chrome:function(){
+            return $.browser()[0] =='Chrome';
+        },
+        opera:function(){
+            return $.browser()[0] =='Opera';
+        },
+        safari:function(){
+            return $.browser()[0] =='Safari';
+        },
+        name:function(){
+            return $.browser()[0];
+        },
+        version:function(){
+            //  防止360se、Sougou Browser等壳浏览器修改UA导致判断不准确
+            if($.browser.msie){
+                var v = 3,
+                div = document.createElement('div'),
+                all = div.getElementsByTagName('i');
+                while (
+                    div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
+                    all[0]
+                );  
+                ieversion = v > 4 ? v : false;
+                if(ieversion){
+                    return ieversion;
+                }
+            }
+            var v_num = $.browser()[1].split("."),
+            v = 0;
+            if(v_num[1]){
+                v = v_num[0] + "." +v_num[1];
+            }
+            else{
+                v = v_num[0] + ".0";
+            }
+
+            return parseInt(v);
+        }
+    });
